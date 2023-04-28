f81e827c5b97345fe6673829b0cb25956867ccb4
kmlgkcy <gmkemalim@gmail.com>
2023-04-28 19:10:38 +0300

    App: main view updated

--- www/lib/feature/main/view/main_view.dart
+++ www/lib/feature/main/view/main_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../../../gen/translations.g.dart';
 import '../../../utils/widget/app_bar.dart';
 
 class MainView extends StatefulWidget {
@@ -14,8 +16,24 @@ class _MainViewState extends State<MainView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CommonWidgets.appbar(context),
-      body: const Center(
-        child: Text('Main Page'),
+      body: Center(
+        child: SizedBox(
+          width: 800,
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              Text(t.pages.main.about),
+              const SizedBox(height: 80),
+              Text(t.pages.main.project_page),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () async {
+                    await launchUrl(Uri.parse('https://github.com/kmlgkcy/fson'));
+                  },
+                  child: const Text('GitHub'))
+            ],
+          ),
+        ),
       ),
     );
   }
